988290b9574f2329e4d37dbe68262381671de3b4
Maryna Berezhanska <marina@MacBook-Pro-Marina.local>
2022-04-24 21:06:34 +0300

    first commit

--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const getInputEl = document.getElementById('font-size-control');
-const getOutputEl = document.getElementById('text');
-
-getInputEl.addEventListener('input', onInputChange);
-
-function onInputChange(){ 
-    getOutputEl.style.fontSize = `${getInputEl.value}px`;
-};
-
