3f24933d3b640d0a20117c30985e4457e566ac12
Arina-Lex-2012 <arina.lex2012@gmail.com>
2022-05-05 10:58:10 +0200

    02-lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,95 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+  
+const elements = galleryItems.map((item) => {
+  const galleryItemEl = document.createElement('div');
+  galleryItemEl.classList.add("gallery__item");
+
+  const galleryLinkEl = document.createElement('a');
+  galleryLinkEl.classList.add('gallery__link');
+  galleryLinkEl.href = `${item.original}`;
+  
+
+  const galleryImageEl = document.createElement('img');
+  galleryImageEl.classList.add('gallery__image');
+  galleryImageEl.src = `${item.preview}`;
+  galleryImageEl.setAttribute('data-source', `${item.original}`);
+  galleryImageEl.alt = `${item.description}`;
+  
+  galleryLinkEl.append(galleryImageEl);
+  galleryItemEl.append(galleryLinkEl);
+
+  return galleryItemEl;
+});
+
+galleryEl.append(...elements);
+
+
+const onClickItemGallery = function (event) {
+  event.preventDefault();
+  let currentUrl;
+
+  if (event.target.tagName === 'IMG') {
+    currentUrl = event.target.dataset.source;
+
+    const modalEl = basicLightbox.create(`
+    <div class="modal">
+        <a><img src="${currentUrl}"></a>
+    </div>
+`, {
+      onShow: (modalEl) => {
+    modalEl.element().querySelector('a').onclick = modalEl.close;
+      }
+    });
+
+    modalEl.show();
+  };
+};
+
+galleryEl.addEventListener('click', onClickItemGallery);
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < galleryItems.length; i += 1){
+//   const galleryEl = document.querySelector('.gallery');
+
+//   const galleryItemEl = document.createElement('div');
+//   galleryItemEl.classList.add("gallery_item");
+
+
+//   const galleryLinkEl = document.createElement('a');
+//   galleryLinkEl.classList.add('gallery_link');
+//   galleryLinkEl.href = `${galleryItems[i].original}`;
+
+//   const galleryImageEl = document.createElement('img');
+//   galleryImageEl.classList.add('gallery_image');
+//   galleryImageEl.src = `${galleryItems[i].preview}`;
+//   galleryImageEl.setAttribute('data-source', `${galleryItems[i].preview}`);
+//   galleryImageEl.alt = `${galleryItems[i].description}`;
+
+//   galleryItemEl.append(galleryLinkEl);
+
+//   galleryEl.append(galleryItemEl);
+
+// }
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+  
+const elements = galleryItems.map((item) => {
+  const galleryItemEl = document.createElement('a');
+  galleryItemEl.classList.add("gallery__item");
+  galleryItemEl.href = `${item.original}`;
+  
+  const galleryImageEl = document.createElement('img');
+  galleryImageEl.classList.add('gallery__image');
+  galleryImageEl.src = `${item.preview}`;
+  galleryImageEl.alt = `${item.description}`;
+ 
+  galleryItemEl.append(galleryImageEl);
+
+  return galleryItemEl;
+});
+
+galleryEl.append(...elements);
+
+const onClickItemGallery = function (event) {
+    event.preventDefault();
+    let currentUrl;
+
+    if (event.target.tagName === 'IMG') {
+        currentUrl = event.target.closest('a').href;
+
+        const modalEl = new SimpleLightbox('.gallery a', { /* options */ });
+    };
+};
+
+galleryEl.addEventListener('click', onClickItemGallery);
+
+
+
